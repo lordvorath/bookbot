f35829854d2ab2209f65df228f043342ecccb83a
4kill3 <4kill3@gmail.com>
2024-12-23 19:05:18 +0100

    Added report printing

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    print_report(book_path)
+    
 def count_words(book):
     words = book.split()
     return len(words)
@@ -17,12 +21,18 @@ def get_file_contents(path):
         file_contents = f.read()
     return file_contents
 
-def main():
-    book_path = "books/frankenstein.txt"
+def print_report(book_path):
+    print(f"--- Begin report of {book_path} ---")
     file_contents = get_file_contents(book_path)
     word_count = count_words(file_contents)
-    print(word_count)
-    char_count = count_characters(file_contents)
-    print(char_count)
+    print(f"{word_count} words found in the document")
+    print()
+    chars = count_characters(file_contents)
+    for key in sorted(chars):
+        if key.isalpha():
+            print(f"The '{key}' character was found {chars[key]} times")
+    print("--- End report ---")
+
+
 
 main()
