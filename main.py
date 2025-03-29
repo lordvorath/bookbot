1695db65d8c9ba86109bebb56ec294c14d93a066
Stefano Trentin <4kill3@gmail.com>
2025-03-29 09:38:22 +0100

    Make report

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-from stats import count_words
+from stats import count_words, count_chars, sort_dicts
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
     
 def main():
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
     txt = get_book_text("books/frankenstein.txt")
     count = count_words(txt)
-    print(f"{count} words found in the document")
+    print("----------- Word Count ----------")
+    print(f"Found {count} total words")
+    chars = count_chars(txt)
+    print ("--------- Character Count -------")
+    lst = sort_dicts(chars)
+    for i in lst:
+        if not i['key'].isalpha():
+            continue
+        print(f"{i['key']}: {i['val']}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,19 @@
 def count_words(txt):
     return len(txt.split())
+
+def count_chars(txt):
+    txt = txt.lower()
+    chars = {}
+    for c in txt:
+        if c not in chars.keys():
+            chars[c] = 1
+        else:
+            chars[c] += 1
+    return chars
+
+def sort_dicts(chars):
+    lst = []
+    for key, val in chars.items():
+        lst.append({'key' : key, 'val' : val})
+    lst.sort(key=lambda x: x['val'], reverse=True)
+    return lst
